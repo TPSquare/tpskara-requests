2dddeb6152d2ffb22df06be998e2fc11c1022681
ThanhPhu <132542965+TPSquare@users.noreply.github.com>
2025-08-07 08:32:31 +0700

    Update parse-request.js

--- utilities/parse-request.js
+++ utilities/parse-request.js
@@ -2,7 +2,7 @@ export default function parseRequest(rawRequest, status) {
   const props = rawRequest.split("|").map((e) => e.trim());
   return {
     id: props[0],
-    request: status === "paused" && props[2] ? props[1] : undefined,
+    request: status !== "paused" || props[2] ? props[1] : undefined,
     other: props[2] || props[1],
   };
 }
