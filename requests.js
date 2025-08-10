ea77cc5c85d4d1ae88b09cd518b45597a5243b78
ThanhPhu <132542965+TPSquare@users.noreply.github.com>
2025-08-10 10:55:01 +0700

    Update requests.js

--- requests.js
+++ requests.js
@@ -1,10 +1,9 @@
 export default {
   doing: [
-    { youtubeID: "nt5ZYiJtDzU" }, // [HiddenGem Mixtape] 4. Một Vòng Sài Gòn - B Ray
+    { youtubeID: "hqAxbmtzp5s", request: "Bình thường & Có hook" }, // "(FYILY) 6. BẢN NHẠC CUỐI (cho em) - B Ray"
   ],
   paused: [],
   noted: [
-    { youtubeID: "hqAxbmtzp5s", request: "Bình thường & Có hook" }, // "(FYILY) 6. BẢN NHẠC CUỐI (cho em) - B Ray"
     { youtubeID: "H--bME9B2Ow" }, // ĐÊM NAY CÔ ĐƠN | Huỳnh Công Hiếu x Ngắn x Xám | "Đà Lạt Ep2"
     { youtubeID: "N6fhy5HFN1k" }, // 9.Sống Hiphop - Rocky CDE ft @goianhlagio | “Midas Touch” the album
     { youtubeID: "3YtU8BsAPkg" }, // Về Đi Em | B RAY | Official Lyrics Video
@@ -17,7 +16,7 @@ export default {
     { youtubeID: "Li-u6TLonmY" }, // Ricky Star - Nhạc Gây Nghiện [ĐÔI CHÂN ĐI ALBUM],
     { youtubeID: "sz8bmtkX1kY" }, // GIÀU NHẤT KHU PHÙ HOA | JustaTee, Wxrdie, Gill, Dangrangto, Robber | Album WCAs 2024
     { youtubeID: "cBuLHQR6Sq4" }, // KILIAN - VẾT XƯỚC (ft. COOLKID, ZOIE) | Official Visualizer,
-    { youtubeID: "vbqUvaQKFik" }, // Đừng Đổ Lỗi Bọn Trẻ | B Ray x Hoàng Yến Chibi | Official Lyrics Video,
+    { youtubeID: "vbqUvaQKFik", request: "Bình thường & Có hook" }, // Đừng Đổ Lỗi Bọn Trẻ | B Ray x Hoàng Yến Chibi | Official Lyrics Video,
     { youtubeID: "ulSRJhkfDmo", request: "Bình thường & Có hook" }, // [FYILY] 1.BẢN NHẠC BUỒN | B RAY | OFFICIAL VIDEO
     { youtubeID: "3PrZRWJPrG4" }, // Hoa Hồng Trên Bê Tông - RichChoi ft. Rio (Prod. Slime7) | official visualizer (from 'HHTBT' album),
     { youtubeID: "cJejXRG2a-A" }, // NKAY - TỪ NGÀY CÙNG ĐƯỜNG | WHY NOT HIPHOP ? 2025 (Chặng 1)
